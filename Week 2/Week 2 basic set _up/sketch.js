f69832ad018610eb15b352f05b23d925845704d6
olla2009 <olinlablans09@gmail.com>
2026-09-10 14:53:18 +0200

    begonnen met weektaak 2

---
+++ Week 2/Week 2 basic set _up/sketch.js
@@ -0,0 +1,54 @@
+function setup() {
+    createCanvas(800, 600);
+}
+
+function draw() {
+    background(136, 206, 235);
+
+    //bergen
+
+    //berg 1
+    push();
+    translate(200, 100);
+    fill(64, 64, 64)
+    noStroke();
+    triangle(100, 400, 500, 400, 300, 100);
+    pop();
+
+    //berg 2
+    push();
+    translate(0, 100);
+    fill(95, 95, 95)
+    noStroke();
+    triangle(300, 400, 500, 400, 400, 200);
+    pop();
+
+
+
+    //gras
+    fill(0, 128, 0);
+    stroke(0, 100, 0);
+    strokeWeight(5);
+    rect(0, 490, 800, 10);
+
+    //weg
+    fill(95, 95, 95);
+    stroke(80, 80, 80);
+    strokeWeight(5);
+    rect(0, 500, 800, 100,);
+
+    //outline weer normaal
+    strokeWeight(0);
+
+    //strepen van de weg
+    fill(192, 192, 192);
+    stroke(110, 110, 110);
+    strokeWeight(2);
+    rect(-10, 540, 100, 15, 20);
+    rect(150, 540, 100, 15, 20);
+    rect(310, 540, 100, 15, 20);
+    rect(310, 540, 100, 15, 20);
+    rect(470, 540, 100, 15, 20);
+    rect(630, 540, 100, 15, 20);
+    rect(790, 540, 100, 15, 20);
+}
