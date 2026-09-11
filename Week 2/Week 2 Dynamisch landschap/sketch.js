0bddc04e1ae7c2677bf254bf08a851472a75e216
olla2009 <olinlablans09@gmail.com>
2026-09-11 14:56:56 +0200

    verder met weektaak 2

--- Week 2/Week 2 Dynamisch landschap/sketch.js
+++ Week 2/Week 2 Dynamisch landschap/sketch.js
@@ -1,8 +1,26 @@
+//variabelen
+let move = 0
+
+
 function setup() {
     createCanvas(800, 600);
 }
 
 function draw() {
+
+
+
+
+
+    //var
+
+    //snelheid
+    move += 0.1 * deltaTime;
+    //terug/loopen
+    if (move > 700) {
+        move = -350;
+    }
+
     background(136, 206, 235);
 
     //bergen
@@ -53,19 +71,71 @@ function draw() {
     rect(790, 540, 100, 15, 20);
 
 
+    //stoplicht
+
+    //functionaliteit
+
+
+
+    //tekst
+    push()
+    translate(430, 285, -10)
+    noStroke()
+
+    //grijs(stok)
+    fill(113, 121, 126)
+    rect(265, 140, 10, 80)
+    rect(250, 20, 40, 120)
+
+
+    //rood
+    fill(255, 0, 0)
+    circle(270, 40, 30)
+
+    //oranje
+    fill(255, 128, 0)
+    circle(270, 80, 30)
+
+    //groen
+    fill(0, 255, 0)
+    circle(270, 40 * 3, 30)
+    pop()
+
+
 
     //auto
     noStroke()
+    push()
+    translate(0 + move, 0, 0)
 
     //body
     fill(0, 0, 255)
     rect(175, 525, 150, 50)
-    rect(175, 450, 100, 50)
+    rect(175, 480, 100, 50)
+
+
+    // tire
+    fill(64)
+    circle(600, 570, 50)
+    circle(700, 570, 50)
+    pop()
+
+
+    //auto2
+    noStroke()
+    push()
+    translate(0 + move, 0, 0)
+
+    //body
+    fill(0, 0, 255)
+    rect(575, 525, 150, 50)
+    rect(575, 480, 100, 50)
+
 
     // tire
     fill(64)
     circle(200, 570, 50)
     circle(300, 570, 50)
-
+    pop()
 
 }
