5a804034fd4b0d24d3e65b8a74a524a7ecbf4d35
olla2009 <olinlablans09@gmail.com>
2026-09-18 10:12:39 +0200

    Big update
    Ik heb gezorgd dat alles nu in het Engels staat want ik gebruikte hiervoor Engels en Nederlands door elkaar

--- Week 2/Week 2 Dynamisch landschap/sketch.js
+++ Week 2/Week 2 Dynamisch landschap/sketch.js
@@ -1,52 +1,51 @@
-//variabelen
+// VARIABLES
 
-//UFO
-let move_UFO = -5000;
+// UFO
+let move_ufo = -5000;
 
-//auto's
-let Minimal_space = 400;
-let move_auto_1 = 0;
-let move_auto_2 = 0;
-let move_auto_3 = 0;
-let move_auto_4 = 0;
+// Cars
+let min_space = 400;
+let move_car_1 = 0;
+let move_car_2 = 0;
+let move_car_3 = 0;
+let move_car_4 = 0;
 
-//zon
+// Sun
 let move_sun = 0;
-let pulsing_sun = 0;
+let pulse_sun = 0;
 
-//wolken
-let move_wolk1 = 0;
-let move_wolk2 = 0;
-let move_wolk3 = 0;
+// Clouds
+let move_cloud_1 = 0;
+let move_cloud_2 = 0;
+let move_cloud_3 = 0;
 
 let random_color = 0;
 
-//timer/stoplicht
+// Timer and Traffic light
 let timer = 0;
-let statusStoplicht = "groen";
+let traffic_light_status = "green";
 
 function keyPressed() {
     if (keyCode == 13) {
-        if (statusStoplicht == "groen") {
-            statusStoplicht = "oranje";
+        if (traffic_light_status == "green") {
+            traffic_light_status = "orange";
             return;
         }
-        if (statusStoplicht == "oranje") {
-            statusStoplicht = "rood";
+        if (traffic_light_status == "orange") {
+            traffic_light_status = "red";
             return;
         }
-        if (statusStoplicht == "rood") {
-            statusStoplicht = "groen";
+        if (traffic_light_status == "red") {
+            traffic_light_status = "green";
             return;
         }
     }
 }
 
-
 function setup() {
     createCanvas(1000, 800);
 
-    //random color
+    // Random color
     random_color = 0 + random(140, 255);
 }
 
@@ -59,91 +58,83 @@ function draw() {
         timer = 0;
     }
 
-    //UFO loopen
-    move_UFO += 0.1 * 200
-    if (move_UFO > 10000) {
-        move_UFO = -750;
+    // UFO loop
+    move_ufo += 0.1 * 200;
+    if (move_ufo > 10000) {
+        move_ufo = -750;
     }
 
-    //auto's loopen
-    if (move_auto_3 > 1100) {
-        move_auto_3 = min(move_auto_4 - Minimal_space, -850);
+    // Cars loop
+    if (move_car_3 > 1100) {
+        move_car_3 = min(move_car_4 - min_space, -850);
     }
 
-
-    if (move_auto_4 > 1100) {
-        move_auto_4 = min(move_auto_3 - Minimal_space, -850);
+    if (move_car_4 > 1100) {
+        move_car_4 = min(move_car_3 - min_space, -850);
     }
 
-
-
-    if (move_auto_1 > 1100) {
-        move_auto_1 = min(move_auto_2 - Minimal_space, -850);
+    if (move_car_1 > 1100) {
+        move_car_1 = min(move_car_2 - min_space, -850);
     }
 
-
-    if (move_auto_2 > 1100) {
-        move_auto_2 = min(move_auto_1 - Minimal_space, -850);
+    if (move_car_2 > 1100) {
+        move_car_2 = min(move_car_1 - min_space, -850);
     }
 
+    // Clouds
+    // Speed
+    move_cloud_1 += 0.1 * 10;
 
-
-
-    //wolken
-
-    //snelheid
-    move_wolk1 += 0.1 * 10;
-
-    //terug loopen
-    if (move_wolk1 > 400) {
-        move_wolk1 = -950;
+    // Loop back
+    if (move_cloud_1 > 400) {
+        move_cloud_1 = -950;
     }
 
-    move_wolk2 += 0.1 * 15;
-    if (move_wolk2 > 800) {
-        move_wolk2 = -950;
+    move_cloud_2 += 0.1 * 15;
+    if (move_cloud_2 > 800) {
+        move_cloud_2 = -950;
     }
 
-    move_wolk3 += 0.1 * 20;
-    if (move_wolk3 > 900) {
-        move_wolk3 = -950;
+    move_cloud_3 += 0.1 * 20;
+    if (move_cloud_3 > 900) {
+        move_cloud_3 = -950;
     }
 
-    //zon 
+    // Sun 
     move_sun += 0.05 * deltaTime;
-    //terug loopen
+    // Loop back
     if (move_sun > 400) {
         move_sun = 60;
     }
 
-    //zon pulseren 
-    pulsing_sun += 0.1 * deltaTime;
-    //terug/loopen
-    if (pulsing_sun > 100) {
-        pulsing_sun = 0;
+    // Sun pulse 
+    pulse_sun += 0.1 * deltaTime;
+    // Loop back
+    if (pulse_sun > 100) {
+        pulse_sun = 0;
     }
 
-    //bladeren bewegen
-    let Move_leaves_1 = 4 * sin(frameCount * 0.03)
-    let Move_leaves_2 = 7 * sin(frameCount * 0.03 + 0.5)
-    let Move_leaves_3 = 10 * sin(frameCount * 0.03 + 1.0)
-    let Move_leaves_4 = 13 * sin(frameCount * 0.03 + 1.5)
+    // Leaves moving
+    let move_leaves_1 = 4 * sin(frameCount * 0.03);
+    let move_leaves_2 = 7 * sin(frameCount * 0.03 + 0.5);
+    let move_leaves_3 = 10 * sin(frameCount * 0.03 + 1.0);
+    let move_leaves_4 = 13 * sin(frameCount * 0.03 + 1.5);
 
-    //zon
+    // Sun
     push();
     fill(255, 165, 0, 70);
-    circle(150 + move_sun, 100, 100 + pulsing_sun);
-    circle(150 + move_sun, 100, 200 - pulsing_sun);
+    circle(150 + move_sun, 100, 100 + pulse_sun);
+    circle(150 + move_sun, 100, 200 - pulse_sun);
     fill(255, 255, 0);
     circle(150 + move_sun, 100, 70);
     pop();
 
-    //wolken 
+    // Clouds 
 
-    //wolk 1
+    // Cloud 1
     push();
     fill(200);
-    translate(0 - move_wolk1, 0, 0);
+    translate(0 - move_cloud_1, 0, 0);
     push();
     translate(0, -10, 0);
     circle(120, 80, 70);
@@ -156,10 +147,10 @@ function draw() {
     circle(160, 85, 50);
     pop();
 
-    //wolk2
+    // Cloud 2
     push();
     fill(200);
-    translate(500 - move_wolk2, 50, 0);
+    translate(500 - move_cloud_2, 50, 0);
     push();
     translate(0, -10, 0);
     circle(120, 80, 70);
@@ -172,10 +163,10 @@ function draw() {
     circle(160, 85, 50);
     pop();
 
-    //wolk3
+    // Cloud 3
     push();
     fill(200);
-    translate(850 - move_wolk3, 150, 0);
+    translate(850 - move_cloud_3, 150, 0);
     push();
     translate(0, -10, 0);
     circle(120, 80, 70);
@@ -188,96 +179,87 @@ function draw() {
     circle(160, 85, 50);
     pop();
 
-    // BERGEN
+    // MOUNTAINS
     push();
     translate(100, 190, 0);
     stroke(0);
     strokeWeight(1);
 
-    // 2. Middelste berg
+    // 2. Middle mountain
     fill(64, 64, 64);
     triangle(280, 500, 750, 500, 500, 190);
 
-    // 1. Linker berg
+    // 1. Left mountain
     fill(95, 95, 95);
     triangle(120, 500, 380, 500, 240, 340);
 
-    // 3. Rechter berg
+    // 3. Right mountain
     fill(95, 95, 95);
     triangle(590, 500, 950, 500, 800, 360);
     pop();
 
+    // Trees in background
 
-    // bomen achter
+    // Trunks
+    fill(107, 73, 43);
 
-    //stamen
-    fill(107, 73, 43)
+    rect(190, 550, 20, 150);
+    rect(490, 550, 20, 150);
+    rect(850, 550, 20, 150);
 
-    rect(190, 550, 20, 150)
-    rect(490, 550, 20, 150)
-    rect(850, 550, 20, 150)
+    // Leaves
+    fill(120, 200, 85);
+    circle(190 + move_leaves_1, 550, 80);
 
-    //bladeren
-    fill(120, 200, 85)
-    circle(190 + Move_leaves_1, 550, 80)
+    fill(78, 169, 59);
+    circle(190 + move_leaves_2, 550, 80);
 
-    fill(78, 169, 59)
-    circle(190 + Move_leaves_2, 550, 80)
+    fill(47, 127, 52);
+    circle(190 + move_leaves_3, 550, 80);
 
-    fill(47, 127, 52)
-    circle(190 + Move_leaves_3, 550, 80)
+    fill(27, 77, 34);
+    circle(190 + move_leaves_4, 550, 80);
 
-    fill(27, 77, 34)
-    circle(190 + Move_leaves_4, 550, 80)
+    fill(120, 200, 85);
+    circle(490 + move_leaves_1, 550, 84);
 
+    fill(78, 169, 59);
+    circle(490 + move_leaves_2, 550, 84);
 
-    fill(120, 200, 85)
-    circle(490 + Move_leaves_1, 550, 84)
+    fill(47, 127, 52);
+    circle(490 + move_leaves_3, 550, 84);
 
+    fill(27, 77, 34);
+    circle(490 + move_leaves_4, 550, 84);
 
-    fill(78, 169, 59)
-    circle(490 + Move_leaves_2, 550, 84)
+    fill(120, 200, 85);
+    circle(850 + move_leaves_1, 550, 84);
 
-    fill(47, 127, 52)
-    circle(490 + Move_leaves_3, 550, 84)
+    fill(78, 169, 59);
+    circle(850 + move_leaves_2, 550, 84);
 
+    fill(47, 127, 52);
+    circle(850 + move_leaves_3, 550, 84);
 
-    fill(27, 77, 34)
-    circle(490 + Move_leaves_4, 550, 84)
+    fill(27, 77, 34);
+    circle(850 + move_leaves_4, 550, 84);
 
-
-    fill(120, 200, 85)
-    circle(850 + Move_leaves_1, 550, 84)
-
-
-    fill(78, 169, 59)
-    circle(850 + Move_leaves_2, 550, 84)
-
-    fill(47, 127, 52)
-    circle(850 + Move_leaves_3, 550, 84)
-
-
-    fill(27, 77, 34)
-    circle(850 + Move_leaves_4, 550, 84)
-
-
-
-    //gras
+    // Grass
     fill(0, 128, 0);
     stroke(0, 100, 0);
     strokeWeight(5);
     rect(0, 690, 1000, 10);
 
-    //weg
+    // Road
     fill(95, 95, 95);
     stroke(80, 80, 80);
     strokeWeight(5);
     rect(0, 700, 1000, 100);
 
-    //outline weer normaal
+    // Reset outline
     strokeWeight(0);
 
-    //strepen van de weg
+    // Road lines
     push();
     translate(0, 200);
     fill(192, 192, 192);
@@ -291,65 +273,63 @@ function draw() {
     rect(790, 540, 100, 15, 20);
     pop();
 
-    //stoplicht
+    // Traffic light
     push();
     translate(650, 485, -10);
     noStroke();
 
-    //stok zwart
+    // Black pole
     fill(0, 0, 0);
     rect(265, 140, 10, 80);
     rect(250, 20, 40, 120);
 
-    //rood
+    // Red light off
     fill(100, 0, 0);
     circle(270, 40, 30);
 
-    //oranje
+    // Orange light off
     fill(150, 100, 0);
     circle(270, 80, 30);
 
-    //groen
+    // Green light off
     fill(0, 100, 0);
     circle(270, 120, 30);
 
-
-    if (statusStoplicht == "groen") {
+    if (traffic_light_status == "green") {
         fill(13, 219, 13);
         circle(270, 120, 30);
 
-        move_auto_1 += 0.1 * 50;
-        move_auto_2 += 0.1 * 50;
-        move_auto_3 += 0.1 * 100;
-        move_auto_4 += 0.1 * 100;
+        move_car_1 += 0.1 * 50;
+        move_car_2 += 0.1 * 50;
+        move_car_3 += 0.1 * 100;
+        move_car_4 += 0.1 * 100;
     }
 
-    if (statusStoplicht == "oranje") {
+    if (traffic_light_status == "orange") {
         fill(255, 102, 0);
         circle(270, 80, 30);
 
-        move_auto_1 += 0.1 * 15;
-        move_auto_2 += 0.1 * 15;
-        move_auto_3 += 0.1 * 30;
-        move_auto_4 += 0.1 * 30;
+        move_car_1 += 0.1 * 15;
+        move_car_2 += 0.1 * 15;
+        move_car_3 += 0.1 * 30;
+        move_car_4 += 0.1 * 30;
     }
 
-    if (statusStoplicht == "rood") {
+    if (traffic_light_status == "red") {
         fill(255, 0, 0);
         circle(270, 40, 30);
     }
 
     pop();
 
-    //auto boven 1
+    // Top car 1
     push();
-    translate(0 + move_auto_3, -60, 0);
+    translate(0 + move_car_3, -60, 0);
 
-    //body
+    // Body
     fill(random_color, 20, 50);
 
-
-    //body
+    // Body
     fill(random_color, 100, 80);
     rect(575, 630, 100, 50);
     rect(575, 725, 150, 50);
@@ -359,122 +339,110 @@ function draw() {
     rect(470, 725, 160, 50);
     rect(470, 680, 110, 50);
 
-    // tire
+    // Tires
     fill(64);
     circle(700, 770, 50);
     circle(620, 770, 50);
     circle(490, 770, 50);
     circle(560, 770, 50);
 
-    //windows
+    // Windows
     strokeWeight(5);
-    stroke("black")
-    fill(144, 213, 255)
+    stroke("black");
+    fill(144, 213, 255);
     rect(480, 660, 80, 50, 20);
     rect(580, 660, 80, 50, 20);
     pop();
 
-    //auto boven 2
+    // Top car 2
     push();
-    translate(-300 + move_auto_4, -60, 0);
+    translate(-300 + move_car_4, -60, 0);
 
-    //body
+    // Body
     fill(random_color, random_color, 0);
     rect(575, 725, 150, 50);
     rect(575, 680, 100, 50);
 
-    //hat
-    fill(0, 0, random_color,)
-    rect(575, 655, 100, 25, 0)
-    rect(575, 645, 100, 25, 100)
+    fill(0, 0, random_color);
+    rect(575, 655, 100, 25, 0);
+    rect(575, 645, 100, 25, 100);
 
-    // tire
+    // Tires
     fill(64);
     circle(700, 770, 50);
     circle(600, 770, 50);
     pop();
 
-    //auto onder 1
+    // Bottom car 1
     noStroke();
     push();
-    translate(-200 + move_auto_2, 0, 0);
+    translate(-200 + move_car_2, 0, 0);
 
-    //body
+    // Body
     fill(random_color, 0, random_color);
     rect(175, 725, 150, 50);
     rect(175, 680, 100, 50);
 
-    // tire
+    // Tires
     fill(64);
     circle(200, 770, 50);
     circle(300, 770, 50);
     pop();
 
-    //auto onder 2
+    // Bottom car 2
     push();
-    translate(0 + move_auto_1, 0, 0);
+    translate(0 + move_car_1, 0, 0);
 
-    //body
+    // Body
     fill(0, random_color, random_color);
     rect(575, 725, 150, 50);
     rect(575, 680, 100, 50);
 
-    // tire
+    // Tires
     fill(64);
     circle(700, 770, 50);
     circle(600, 770, 50);
     pop();
 
+    // UFO
+    push();
+    translate(move_ufo, 0, 0);
 
-    //ufo
-    push()
-    translate(move_UFO, 0, 0)
+    fill(0, 255, 0);
+    ellipse(150, 160, 25, 40);
 
-    fill(0, 255, 0)
-    ellipse(150, 160, 25, 40,)
+    fill(0, 0, 0);
+    rect(145, 150, 10, 10, 100);
 
-    fill(0, 0, 0)
-    rect(145, 150, 10, 10, 100)
+    fill(0, 0, 255, 90);
+    circle(150, 180, 80);
 
-    fill(0, 0, 255, 90)
-    circle(150, 180, 80)
+    fill(150, 150, 150);
+    ellipse(150, 200, 200, 50);
 
+    fill(0, 0, 255);
+    ellipse(150, 220, 25, 10);
 
-    fill(150, 150, 150)
-    ellipse(150, 200, 200, 50,)
+    fill(0, 0, 0);
+    rect(50, 195, 200, 10, 100);
+    pop();
 
-    fill(0, 0, 255)
-    ellipse(150, 220, 25, 10,)
+    // Tree in front
 
-    fill(0, 0, 0)
-    rect(50, 195, 200, 10, 100)
-    pop()
+    // Trunk
+    fill(107, 73, 43);
+    rect(45, 660, 20, 150);
 
+    // Leaves
+    fill(120, 200, 85);
+    circle(40 + move_leaves_1, 650, 90);
 
+    fill(78, 169, 59);
+    circle(40 + move_leaves_2, 650, 90);
 
+    fill(47, 127, 52);
+    circle(40 + move_leaves_3, 650, 90);
 
-
-    //boom voor
-
-    //stam
-    fill(107, 73, 43)
-    rect(45, 660, 20, 150)
-
-    //bladeren
-
-    fill(120, 200, 85)
-    circle(40 + Move_leaves_1, 650, 90)
-
-
-    fill(78, 169, 59)
-    circle(40 + Move_leaves_2, 650, 90)
-
-    fill(47, 127, 52)
-    circle(40 + Move_leaves_3, 650, 90)
-
-
-    fill(27, 77, 34)
-    circle(40 + Move_leaves_4, 650, 90)
-    line(mouseX, 100, mouseX, 100);
-
+    fill(27, 77, 34);
+    circle(40 + move_leaves_4, 650, 90);
 }
--- Week 3/week 3 Boter, Kaas, en Eieren/sketch.js
+++ Week 3/week 3 Boter, Kaas, en Eieren/sketch.js
@@ -1,7 +1,48 @@
+//variabelen
+let rectX = 100;
+let rectY = 100;
+let rectW = 200;
+let rectH = 100;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 600);
 }
 
 function draw() {
-  background(220);
+  //functionaliteit
+  background(240);
+
+
+
+
+
+
+
+
+
+
+  //bord
+  fill(0, 0, 0)
+  rect(50, 100, 400, 400, 50)
+
+  //vakjes van het bord
+  fill(160, 160, 160)
+
+  //vakjes boven 
+  rect(80, 120, 100, 100, 10)
+  rect(200, 120, 100, 100, 10)
+  rect(320, 120, 100, 100, 10)
+
+  //vakjes midden
+  rect(80, 240, 100, 100, 10)
+  rect(200, 240, 100, 100, 10)
+  rect(320, 240, 100, 100, 10)
+
+  //vakjes onder
+  rect(80, 360, 100, 100, 10)
+  rect(200, 360, 100, 100, 10)
+  rect(320, 360, 100, 100, 10)
+
+  circle(mouseX, mouseY, 30);
 }
